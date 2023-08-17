c0e280fdfda8fcd664e82d8799a489b3f1942090
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2023-08-17 19:42:33 -0400

    build(react-components): fix type exports

---
+++ packages/react-components/src/index.js
@@ -0,0 +1,47 @@
+import AdditionalInfo from './components/AdditionalInfo';
+import AlertBox, { ALERT_TYPE } from './components/AlertBox';
+import Breadcrumbs from './components/Breadcrumbs';
+import Checkbox from './components/Checkbox';
+import ExpandingGroup from './components/ExpandingGroup';
+import IconBase from './components/IconBase';
+import IconSearch from './components/IconSearch';
+import IconUser from './components/IconUser';
+import LoadingIndicator from './components/LoadingIndicator';
+import MaintenanceBanner from './components/MaintenanceBanner';
+import Modal from './components/Modal';
+import OMBInfo from './components/OMBInfo';
+import Pagination from './components/Pagination';
+import Select from './components/Select';
+import Telephone, {
+  CONTACTS,
+  contactsMap,
+  PATTERNS as PHONE_PATTERNS,
+  patternsMap as PHONE_PATTERNS_MAP,
+} from './components/Telephone';
+import TextInput from './components/TextInput';
+
+import('@department-of-veterans-affairs/core/i18n/i18n-setup');
+
+export {
+  AdditionalInfo,
+  AlertBox,
+  ALERT_TYPE,
+  Breadcrumbs,
+  Checkbox,
+  ExpandingGroup,
+  IconBase,
+  IconSearch,
+  IconUser,
+  LoadingIndicator,
+  MaintenanceBanner,
+  Modal,
+  OMBInfo,
+  Pagination,
+  Select,
+  Telephone,
+  CONTACTS,
+  contactsMap,
+  PHONE_PATTERNS,
+  PHONE_PATTERNS_MAP,
+  TextInput,
+};
